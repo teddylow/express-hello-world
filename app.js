71aa98f1e9e75fc2b73f5a2c68b415949fc1cef8
teddylow <106261706+teddylow@users.noreply.github.com>
2026-01-21 04:17:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -27,11 +27,15 @@ app.get('/', (req, res) => {
 
 // Route for POST requests
 app.post('/', (req, res) => {
-  req.body.redirect(forward_url);
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end()
+  if (res.status === 200) {
+    app.post(forward_url, {
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body)
+    })
 });
 
 // Start the server
